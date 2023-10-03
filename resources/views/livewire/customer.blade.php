0c2675c8793ca84b988ee91e120086ac53f8601f
faulknerpg <30496308+faulknerpg@users.noreply.github.com>
2023-10-03 17:49:56 +0100

    Added some real text

--- resources/views/livewire/customer.blade.php
+++ resources/views/livewire/customer.blade.php
@@ -2,8 +2,11 @@
 
 @section('content')
 
-    <div class="bg-blue-500 h-24 w-24 rounded-tr-3xl rounded-br-lg">
-        The whole world belongs to you.
-        {{-- The whole world belongs to you. --}}
+    <div class="bg-blue-500 p-5 h-80 w-80 aspect-auto rounded-tr-3xl rounded-br-lg">
+    Implementing a Customer Relationship Management (CRM) system
+    can be a difficult but transformative decision for any small
+    business.  However, there are several compelling reasons to invest in
+    such a tool.
     </div>
+
 @endsection
